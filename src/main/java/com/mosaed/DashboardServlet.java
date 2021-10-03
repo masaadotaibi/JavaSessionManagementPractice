81e9c71aaf34d2b71caca1891e4ccbdcb3a9c990
masaadotaibi <mosaed_9999@hotmail.com>
2021-10-03 22:08:33 +0300

    Initializing the project with simple example

---
+++ src/main/java/com/mosaed/DashboardServlet.java
@@ -0,0 +1,59 @@
+package com.mosaed;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class DashboardServlet
+ */
+@WebServlet("/dashboard")
+public class DashboardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DashboardServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+        System.out.println("Reached #3");
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		System.out.println("Reached #5");
+		
+		HttpSession session = req.getSession();
+			
+		String username = (String) session.getAttribute("username");
+		
+		PrintWriter out = res.getWriter();
+		
+		out.println("Hi " + username + ". You had a successful login in " + new Date());
+		
+		session.removeAttribute("username");
+		
+		String removedUser = (String) session.getAttribute("username");
+		
+		out.println("You have left " + removedUser);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/java/com/mosaed/LoginServlet.java
@@ -0,0 +1,54 @@
+package com.mosaed;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		
+		HttpSession session = req.getSession();
+		
+		session.setAttribute("username", username);
+		session.setAttribute("password", password);
+		
+		System.out.println("Reached #1");
+		
+		res.sendRedirect("dashboard");
+		
+		System.out.println("Reached #2");
+	}
+
+}
